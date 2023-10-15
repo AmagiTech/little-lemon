874b9b4e85fddcf9989fbcbabd22f0ea505baa6a
Muharrem Barkın <barkinm@outlook.com>
2023-10-15 16:10:52 +0300

    some design applied

--- src/components/Specials.js
+++ src/components/Specials.js
@@ -6,7 +6,7 @@ import { useState } from 'react';
 
 function Specials(props) {
 
-    let [specialDishes,setSpecialDishes] = useState([{
+    let [specialDishes, setSpecialDishes] = useState([{
         title: 'Greek Salad',
         image: imgGreekSalad,
         subtitle: '$12.99',
@@ -15,31 +15,36 @@ function Specials(props) {
     {
         title: 'Bruchetta',
         image: imgBruchetta,
-        subtitle: '$12.99',
-        text: 'The famous greek salad of crispy lettuce, peppers, olives and our Chicago styles feta cheese, garnished with crunchy garlic and rosemary croutons.'
+        subtitle: '$5.89',
+        text: 'Our Bruschetta is made from grilled bread that has been smeared with garlic and seasdoned with salt and olive oil.'
     },
     {
         title: 'Lemon Dessert',
         image: imgLemonDesert,
-        subtitle: '$12.99',
-        text: 'The famous greek salad of crispy lettuce, peppers, olives and our Chicago styles feta cheese, garnished with crunchy garlic and rosemary croutons.'
+        subtitle: '$5.00',
+        text: "Ths comes straight from grandma's recipe book. every last ingredient has been sourced and is as authentic as can be imagined."
     },
     ]);
 
 
     return (
         <div className="container px-4 py-5">
-            <div className="row">
-                <h1>asdsadas</h1>
-                {specialDishes.map((dish, i)=> {
+            <div className='d-flex justify-content-between'>
+                <h2>This Week Specials</h2>
+                <button className='btn btn-yellow self-align-end'>Online Menu</button>
+            </div>
+
+            <div className='d-flex justify-content-around'>
+                {specialDishes.map((dish, i) => {
                     console.log(dish);
                     return <SpecialCard key={dish.title}
-                    title = {dish.title}
-                    image = {dish.image}
-                    text = {dish.text}
-                    subtitle = {dish.subtitle}/>
+                        title={dish.title}
+                        image={dish.image}
+                        text={dish.text}
+                        subtitle={dish.subtitle} />
                 })}
             </div>
+
         </div>
     );
 }
--- src/components/SpecialsCard.js
+++ src/components/SpecialsCard.js
@@ -1,12 +1,13 @@
+import '../App.css';
 function SpecialCard(props) {
     console.log(props);
-    return (<div className="card col-lg-4" style={{width:"18rem"}} >
+    return (<div className="card col-lg-4 m-2" style={{width:"18rem"}} >
         <img src={props.image} className="card-img-top" alt={props.subtitle} />
         <div className="card-body">
             <h5 className="card-title">{props.title}</h5>
             <h6 className="card-subtitle mb-2 text-body-secondary">{props.subtitle}</h6>
             <p className="card-text">{props.text}</p>
-            <a href="#" className="btn btn-primary">Order Delivery</a>
+            <a href="#" className="btn btn-yellow">Order Delivery</a>
         </div>
     </div >)
 }
